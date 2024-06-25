d9f3e816aaceb7d192ac58de894fbc3c8792d820
saviita <taatt2000@gmail.com>
2024-06-26 01:12:06 +0200

    26/06

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
-import '../scss/styles.scss';
+import "../scss/styles.scss";
 
 //rootStyles.setProperty('--primary-color', 'green');
 
@@ -9,34 +9,40 @@ inputColorElement.addEventListener('input', event => {
 }) */
 
 const rootStyles = document.documentElement.style;
-const sizeElement = document.getElementById('size');
-const rgbElement = document.getElementById('rgb');
-const hexElement = document.getElementById('hex');
+const sizeElement = document.getElementById("size");
+const rgbElement = document.getElementById("rgb");
+const hexElement = document.getElementById("hex");
 
-const getScroll = event => {
-  const size = (window.scrollY * 100) / (document.body.scrollHeight - window.innerHeight);
-  rootStyles.setProperty('--box-size', size + '%');
+const getScroll = (event) => {
+  const size =
+    (window.scrollY * 100) / (document.body.scrollHeight - window.innerHeight);
+  rootStyles.setProperty("--box-size", size + "%");
   sizeElement.textContent = window.scrollY;
 };
-window.addEventListener('scroll', getScroll);
+window.addEventListener("scroll", getScroll);
 
-const getRgb = event => {
+const getRgb = (event) => {
   const red = Math.floor(Math.random() * 256);
   const green = Math.floor(Math.random() * 256);
   const blue = Math.floor(Math.random() * 256);
   const color = `rgb(${red}, ${green}, ${blue})`;
-  rootStyles.setProperty('--random-color', color);
+  rootStyles.setProperty("--random-color", color);
 };
-rgbElement.addEventListener('click', getRgb);
+rgbElement.addEventListener("click", getRgb);
 
-const getHex = event => {
-  const hexaColor = '0123456789abcdef';
-  let color = '#';
-  for (let index = 0; index < 7; index++) {
+const getHex = (event) => {
+  const hexaColor = "0123456789abcdef";
+  let color = "#";
+  for (let index = 0; index < 6; index++) {
     const randomNumber = Math.floor(Math.random() * hexaColor.length);
-    color += randomNumber;
+    color += hexaColor.charAt(randomNumber);
   }
-  rootStyles.setProperty('--random-color', color);
+  rootStyles.setProperty("--random-color", color);
   console.log(color);
 };
-hexElement.addEventListener('click', getHex);
+hexElement.addEventListener("click", getHex);
+
+const mouse = (event) => {
+  console.log(window.innerHeight);
+};
+window.addEventListener("mousemove", mouse);
